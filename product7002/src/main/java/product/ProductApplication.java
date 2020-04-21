46cf3d18a2d965c9149206900a41e9f562870842
chenzhishang <290962663@qq.com>
2020-04-21 10:22:41 +0800

    Revert "第一次提交"
    This reverts commit 263241f5

---
+++ product7002/src/main/java/product/ProductApplication.java
@@ -0,0 +1,16 @@
+package product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker//熔断 降级开启注解
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class,args);
+    }
+}
