93fb6c77f9f53bc4efa71126186e6287b57ebba5
chenzhishang <290962663@qq.com>
2020-04-21 14:48:14 +0800

    Revert "第一次提交"
    This reverts commit 263241f5

---
+++ consum9002/src/main/java/com/wondersgroup/consum/service/ConsumServiceImpl.java
@@ -0,0 +1,38 @@
+package com.wondersgroup.consum.service;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ConsumServiceImpl implements ConsumService{
+
+    @Override
+    public String eureka() {
+        return null;
+    }
+
+    @Override
+    public String openFeign() {
+        return null;
+    }
+
+    @Override
+    public String hystrixDowngrade1() {
+        return null;
+    }
+
+    @Override
+    public String hystrixDowngrade2() {
+        return null;
+    }
+
+    @Override
+    public String hystrixFuse1() {
+        return null;
+    }
+
+    @Override
+    public String hystrixFuse2() {
+        return null;
+    }
+}
